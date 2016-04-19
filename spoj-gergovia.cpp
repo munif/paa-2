954f5e6bdc4a483f3cefa7f9e9aee0ccc27ceb6a
Abdul Munif <abdulmuneverlose@gmail.com>
2016-04-19 07:22:37 +0700

    Add SPOJ-GERGOVIA

---
+++ spoj-gergovia.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int n, cost = 0;
+    int demand[100000];
+
+    cin >> n;
+    cin >> demand[0];
+    for(int i=1; i<n; i++)
+    {
+        cin >> demand[i];
+
+    }
+}
